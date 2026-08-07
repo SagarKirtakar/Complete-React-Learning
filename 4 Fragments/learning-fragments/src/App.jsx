ca74d4821c1f548077aadd2f447677246023f20d
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-08-07 18:55:07 +0530

    learning fragments

---
+++ 4 Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+
+  return <>
+    <h1>Healthy food</h1>
+    <ul class="list-group">
+      <li class="list-group-item">Dal</li>
+      <li class="list-group-item">Chawal</li>
+      <li class="list-group-item">Roti</li>
+      <li class="list-group-item">Sabji</li>
+      <li class="list-group-item">Milkshake</li>
+    </ul>
+  </>
+
+}
+
+export default App
