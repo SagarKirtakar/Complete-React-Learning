be554dfaf32e2ca11fe244ba65be2d390958f69c
Sagar Kirtakar <sagarkirtakar2002@gmail.com>
2026-08-07 19:27:20 +0530

    learning-map-method

--- 4 Fragments/learning-fragments/src/App.jsx
+++ 4 Fragments/learning-fragments/src/App.jsx
@@ -4,16 +4,19 @@ import './App.css'
 
 function App() {
 
-  return <>
-    <h1>Healthy food</h1>
-    <ul class="list-group">
-      <li class="list-group-item">Dal</li>
-      <li class="list-group-item">Chawal</li>
-      <li class="list-group-item">Roti</li>
-      <li class="list-group-item">Sabji</li>
-      <li class="list-group-item">Milkshake</li>
-    </ul>
-  </>
+  let foodItems = ["Dal", "Chawal", "Roti", "Sabji", "Milkshake", "Salad"];
+
+  return (
+    <>
+      <h1>Healthy food</h1>
+      <ul class="list-group">
+        {
+          foodItems.map((item) => (
+            <li class="list-group-item">{item}</li>
+          ))}
+      </ul>
+    </>
+  );
 
 }
 
